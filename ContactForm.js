20dcae6cf12b164a4ab42aeb2dec307a4d71d134
Aga Matyjasik <agus.wroclaw@gmail.com>
2019-07-03 21:17:15 +0200

    Adding files for task 14.7

---
+++ ContactForm.js
@@ -0,0 +1,15 @@
+    
+function ContactForm (contact){
+    return (
+        <form className='contactForm'>
+            <input type='text' placeholder='Imię' value={contact.firstName} />
+            <input type='text' placeholder='Nazwisko' value={contact.lastName} />
+            <input type='email' placeholder='Email' value={contact.email} />
+            <button type='submit'>Dodaj kontakt</button>
+        </form>
+    );
+};
+
+ContactForm.prototype = {
+    contact: React.PropTypes.object.isRequired
+}
