6e8ad8dfeb22079b884158bf7efa91dda8fb8003
Aga Matyjasik <agus.wroclaw@gmail.com>
2019-07-05 21:45:14 +0200

    Adding rest of files for task 14.7

---
+++ components/Contact.js
@@ -0,0 +1,18 @@
+var Contact = React.createClass({
+	propTypes: function() {
+		item: React.PropTypes.object.isRequried;
+	},
+	render: function() {
+		var propsItem = this.props.item;
+		return (
+			<li className='contactItem'>
+				<img src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'} className={'contactImage'} />
+				<div className='labelWrapper'>
+					<p className='contactLabel'>Imię: {propsItem.firstName}</p>
+					<p className='contactLabel'>Nazwisko: {propsItem.lastName}</p>
+					<a href={'mailto:' + propsItem.email}>{propsItem.email}</a>
+				</div>
+			</li>
+		)
+	}
+});
---
+++ components/ContactForm.js
@@ -0,0 +1,16 @@
+var ContactForm = React.createClass({
+	propTypes: {
+		contact: React.PropTypes.object.isRequired,
+	},
+	render: function() {
+		var propsContact = this.props.contact;
+		return (
+			<form className="form">
+				<input type={'text'} placeholder={'Imię'} value={propsContact.firstName} />
+				<input type={'text'} placeholder={'Nazwisko'} value={propsContact.lastName} />
+				<input type={'text'} placeholder={'Email'} value={propsContact.email} />
+				<button type={'submit'}>Dodaj kontakt</button>
+			</form>
+		)
+	}
+});
